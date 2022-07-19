8c4c910e005fea84c7cfa3f51b486e3d088612a3
Chloekkk <skdusdl8804@gmail.com>
2022-07-19 21:06:42 +0900

    solve: Solve 0302

--- nayeon/string/[0302] 유효한 팰린드롬/index.js
+++ nayeon/string/[0302] 유효한 팰린드롬/index.js
@@ -1,7 +1,11 @@
 function solution(string) {
   const str = string.toLowerCase();
   const r_str = str.split("").reverse().join("");
+  console.log(str);
+  console.log(r_str);
   return str === r_str ? "YES" : "NO";
 }
 
-console.log(solution("gooG"));
+let str1 = "found7, time: study; Yduts; emit, 7Dnuof";
+let str = "fuck";
+console.log(solution(str));
