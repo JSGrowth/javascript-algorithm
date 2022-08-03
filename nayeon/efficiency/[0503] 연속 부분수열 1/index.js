46bcf54b2610a32f37b505bb90e2611c75f5f228
Chloekkk <skdusdl8804@gmail.com>
2022-08-04 02:07:20 +0900

    solve: Solve 0503

---
+++ nayeon/efficiency/[0503] 연속 부분수열 1/index.js
@@ -0,0 +1,21 @@
+console.time("time");
+function solution(m, arr) {
+  let answer = 0;
+  let lt = 0;
+  (rt = 0), (sum = 0);
+  const n = arr.length;
+
+  while (lt < n && rt < n) {
+    sum += arr[rt++];
+    if (sum === m) answer++;
+    while (sum > m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
+    }
+  }
+  return answer;
+}
+
+let a = [1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
+console.timeEnd("time");
