bf1cc0f1e66cedec007cf997abb65b3ae10861b5
Chloekkk <skdusdl8804@gmail.com>
2022-08-14 18:30:52 +0900

    fix: Fix 0708

--- nayeon/greedy/[0708] 회의실 배정/index.js
+++ nayeon/greedy/[0708] 회의실 배정/index.js
@@ -3,7 +3,7 @@ function solution(meeting) {
 
   meeting.sort((arr1, arr2) => {
     if (arr1[1] !== arr2[1]) return arr1[1] - arr2[1];
-    else return arr1[0] - arr1[1];
+    else return arr1[0] - arr2[0];
   });
   console.log(meeting);
   let endTime = 0;
@@ -23,4 +23,10 @@ let arr = [
   [4, 6],
   [5, 7],
 ];
+let arr2 = [
+  [3, 3],
+  [1, 3],
+  [2, 3],
+];
 console.log(solution(arr));
+console.log(solution(arr2));
